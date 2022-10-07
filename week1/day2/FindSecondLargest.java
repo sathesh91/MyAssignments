a3bcbcd95171e658b1e9b846c47ad2569133017a
sathesh91 <46997532+sathesh91@users.noreply.github.com>
2022-10-07 21:48:20 +0530

    Add files via upload
    Array largest numbers

---
+++ week1/day2/FindSecondLargest.java
@@ -0,0 +1,18 @@
+package week1.day2;
+
+import java.util.Arrays;
+
+public class FindSecondLargest {
+
+	public static void main(String[] args) {
+		
+		int[] data = {3,2,11,4,6,7};
+		
+		//Arrange the array in ascending order
+		Arrays.sort(data);
+		
+		//Pick the 2nd element from the last and print it
+		System.out.println(data[data.length-2]);
+	}
+
+}
