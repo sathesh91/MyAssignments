5c2218f72670e5c8818caca306af6b03200fa0c0
sathesh91 <46997532+sathesh91@users.noreply.github.com>
2022-10-07 21:46:49 +0530

    Add files via upload
    If loop

---
+++ week1/day2/CheckIfTheNumberIsPositiveOrNegative.java
@@ -0,0 +1,25 @@
+package week1.day2;
+
+public class CheckIfTheNumberIsPositiveOrNegative {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int number = 1;
+		
+		if(number>0)
+		{
+			System.out.println("The input is positive number :" +number);
+		}
+		else if(number<0)
+		{
+			System.out.println("The input is positive number :" +number);
+		}
+		else
+		{
+			System.out.println("The number is neither positive nor negative" +number);
+		}
+
+	}
+
+}
