cb8a1ed415f73c24536a391ed6be3373de9502e5
sathesh91 <46997532+sathesh91@users.noreply.github.com>
2022-10-15 09:30:28 +0530

    Add files via upload
    Selenium Basic Updated

--- week1/day3/SeleniumInitial.java
+++ week1/day3/SeleniumInitial.java
@@ -9,8 +9,8 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 public class SeleniumInitial {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		WebDriverManager.chromedriver().setup();
+
+ 		 WebDriverManager.chromedriver().setup();
 		 ChromeDriver driver=new ChromeDriver();
 		 driver.get("http://leaftaps.com/opentaps/control/main");
 		 driver.manage().window().maximize();
